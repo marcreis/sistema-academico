547dfb4b1b6d9b1025088270b3a4c54d55550749
Marcelo Reis <marcelodosreislima@gmail.com>
2017-07-13 23:28:32 -0300

    Configurando o banco,faltando criar serviço

--- src/dependencies.php
+++ src/dependencies.php
@@ -17,3 +17,13 @@ $container['logger'] = function ($c) {
     $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
     return $logger;
 };
+
+$container['db'] = function ($container) {
+    $capsule = new \Illuminate\Database\Capsule\Manager;
+    $capsule->addConnection($container['settings']['db']);
+
+    $capsule->setAsGlobal();
+    $capsule->bootEloquent();
+
+    return $capsule;
+};
--- src/settings.php
+++ src/settings.php
@@ -1,19 +1,26 @@
 <?php
 return [
     'settings' => [
-        'displayErrorDetails' => true, // set to false in production
+        'displayErrorDetails'    => true, // set to false in production
         'addContentLengthHeader' => false, // Allow the web server to send the content-length header
 
         // Renderer settings
-        'renderer' => [
+        'renderer'               => [
             'template_path' => __DIR__ . '/../templates/',
         ],
-
         // Monolog settings
-        'logger' => [
+        'logger'                 => [
             'name' => 'slim-app',
             'path' => __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
+        ],
+        'db'                     => [
+            'driver'   => 'pgsql',
+            'host'     => 'localhost',
+            'database' => 'portalaluno',
+            'username' => 'postgres',
+            'password' => '1408',
+            'charset'  => 'utf8'
         ],
     ],
 ];
